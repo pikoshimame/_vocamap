54faa0988abed5bb8633a4997b04d4d2c20304a8
pikoshimame <pikoshimame@gmail.com>
2018-02-01 00:36:51 +0900

    update: svg修正、grid修正

--- src/js/maps.js
+++ src/js/maps.js
@@ -5,7 +5,7 @@ import Constants from './constants';
 import MarkerSet from './marker-set';
 
 const getGoogleClusterInlineSvg = (color) => {
-    const encoded = window.btoa(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-100 -100 200 200"><defs><g id="a" transform="rotate(45)"><path d="M0 47A47 47 0 0 0 47 0L62 0A62 62 0 0 1 0 62Z" fill-opacity="0.7"/><path d="M0 67A67 67 0 0 0 67 0L81 0A81 81 0 0 1 0 81Z" fill-opacity="0.5"/><path d="M0 86A86 86 0 0 0 86 0L100 0A100 100 0 0 1 0 100Z" fill-opacity="0.3"/></g></defs><g fill="${color}"><circle r="42"/><use xlink:href="#a"/><g transform="rotate(120)"><use xlink:href="#a"/></g><g transform="rotate(240)"><use xlink:href="#a"/></g></g></svg>`);
+    const encoded = window.btoa(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-100 -100 200 200"><defs><g id="a" transform="rotate(45)"><path d="M0 47A47 47 0 0 0 47 0L62 0A62 62 0 0 1 0 62Z" fill-opacity="0.5"/></g></defs><g fill="${color}"><circle r="42"/><use xlink:href="#a"/><g transform="rotate(120)"><use xlink:href="#a"/></g><g transform="rotate(240)"><use xlink:href="#a"/></g></g></svg>`);
     return (`data:image/svg+xml;base64,${encoded}`);
 };
 
@@ -17,8 +17,15 @@ export default class Maps {
         this.openInfoWindow = new this.maps.InfoWindow();
         this.markerClusterer = new MarkerClusterer(this.map);
         this.markerClusterer.setStyles([
-            { width: 50, height: 50, url: getGoogleClusterInlineSvg('#EB3223') },
+            {
+                width: 50,
+                height: 50,
+                url: getGoogleClusterInlineSvg('#FC5651'),
+                textColor: '#5E1312',
+                textSize: 14
+            }
         ]);
+        this.markerClusterer.setGridSize(10);
     }
     setMarkerSets() {
         fetch(Constants.DATA_URL)
