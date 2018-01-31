bf4f05aa6d5b9b7145092fca6c11c70c70a2ca44
pikoshimame <pikoshimame@gmail.com>
2018-01-31 16:21:10 +0900

    update: add MarkerClusterer

--- src/js/maps.js
+++ src/js/maps.js
@@ -1,7 +1,13 @@
 import 'whatwg-fetch';
 import anchorify from 'anchorify';
+import MarkerClusterer from 'node-js-marker-clusterer';
 import Constants from './constants';
 import MarkerSet from './marker-set';
+
+const getGoogleClusterInlineSvg = (color) => {
+    const encoded = window.btoa(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-100 -100 200 200"><defs><g id="a" transform="rotate(45)"><path d="M0 47A47 47 0 0 0 47 0L62 0A62 62 0 0 1 0 62Z" fill-opacity="0.7"/><path d="M0 67A67 67 0 0 0 67 0L81 0A81 81 0 0 1 0 81Z" fill-opacity="0.5"/><path d="M0 86A86 86 0 0 0 86 0L100 0A100 100 0 0 1 0 100Z" fill-opacity="0.3"/></g></defs><g fill="${color}"><circle r="42"/><use xlink:href="#a"/><g transform="rotate(120)"><use xlink:href="#a"/></g><g transform="rotate(240)"><use xlink:href="#a"/></g></g></svg>`);
+    return (`data:image/svg+xml;base64,${encoded}`);
+};
 
 export default class Maps {
     constructor(maps, element) {
@@ -9,6 +15,10 @@ export default class Maps {
         this.map = new this.maps.Map(element, Constants.MAP_OPTS);
         this.markerSets = [];
         this.openInfoWindow = new this.maps.InfoWindow();
+        this.markerClusterer = new MarkerClusterer(this.map);
+        this.markerClusterer.setStyles([
+            { width: 50, height: 50, url: getGoogleClusterInlineSvg('#EB3223') },
+        ]);
     }
     setMarkerSets() {
         fetch(Constants.DATA_URL)
@@ -27,13 +37,13 @@ export default class Maps {
                     markerSet.infoWindow.setContent(`${content}`
                         + `<section class="infoWindow__contents"><h2 class="infoWindow__title">${element[0]}</h2>`
                         + `<p class="infoWindow__text">${anchorify(String(element[1]).replace(/\r?\n/g, '<br>'), { target: '_blank' })}</p></section>`);
-                    markerSet.marker.setMap(this.map);
                     markerSet.marker.addListener('click', () => {
                         this.openInfoWindow.setContent(markerSet.infoWindow.getContent());
                         this.openInfoWindow.open(this.map, markerSet.marker);
                     });
                     if (!this.markerSets.some((set) => { return set === markerSet; })) {
                         this.markerSets.push(markerSet);
+                        this.markerClusterer.addMarker(markerSet.marker);
                     }
                 });
             });
@@ -41,7 +51,11 @@ export default class Maps {
     getMarkerSet(position) {
         let markerSet;
         if (!this.markerSets.some((set) => {
-            if (set.marker.getPosition().equals(position)) {
+            const distance = google.maps.geometry.spherical.computeDistanceBetween(
+                set.marker.getPosition(),
+                position
+            );
+            if (distance < 10) {
                 markerSet = set;
                 return true;
             }
