ecc58c157a3b228cb3556dba256ad514fafa5e01
pikoshimame <pikoshimame@gmail.com>
2018-02-05 18:28:38 +0900

    update: Register.vue→LinkArea.vueへ名前変更

---
+++ src/components/LinkArea.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="register">
+        <vmap-button class="btn">
+            <router-link to="about">イベントを登録する</router-link>
+        </vmap-button>
+    </div>
+</template>
+
+<script>
+import VmapButton from './Button';
+
+export default {
+    name: 'VmapRegister',
+    components: { VmapButton }
+};
+</script>
+
+<style scoped>
+.register {
+    background-color: #137a7f;
+    text-align: center;
+    padding: 50px 0;
+}
+.btn {
+    display: inline-block;
+}
+</style>
