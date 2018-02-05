193a692bc627be2f6f7fbe0ea9f39da976c60c4b
pikoshimame <pikoshimame@gmail.com>
2018-02-05 12:00:24 +0900

    update: ボタンデザイン変更

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,7 +1,9 @@
 <template>
-    <vmap-button class="register">
-        <router-link to="about">イベントを登録する</router-link>
-    </vmap-button>
+    <div class="register">
+        <vmap-button class="btn">
+            <router-link to="about">イベントを登録する</router-link>
+        </vmap-button>
+    </div>
 </template>
 
 <script>
@@ -16,9 +18,10 @@ export default {
 <style scoped>
 .register {
     background-color: #137a7f;
+    text-align: center;
     padding: 50px 0;
 }
-.register >>> a {
-    margin: 0 auto;
+.btn {
+    display: inline-block;
 }
 </style>
